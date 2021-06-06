e627e2d78944173444126e61e1614332d65ab7e1
teodev1611 <malehurtadoreyes@hotmail.com>
2021-06-06 17:55:39 -0500

    Add the encrypt file function test

--- files/write_json.go
+++ files/write_json.go
@@ -1,7 +1,13 @@
 package files
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"os"
 
@@ -27,4 +33,30 @@ func WriteJsonDB(name, spaces string, empty bool, data ...map[string]interface{}
 	errors.CheckErrors(err)
 	err2 := ioutil.WriteFile(name, file, os.ModePerm)
 	errors.CheckErrors(err2)
+}
+
+func createHash(key string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(key))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func encrypt(data []byte, passphrase string) []byte {
+	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	return ciphertext
+}
+
+func EncryptFile(filename string, data []byte, passphrase string) {
+	f, _ := os.Create(filename)
+	defer f.Close()
+	f.Write(encrypt(data, passphrase))
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/TeoDev1611/darth-db
 
 go 1.16
 
-require github.com/sirupsen/logrus v1.8.1
+require (
+	github.com/matthewhartstonge/argon2 v0.1.4
+	github.com/sirupsen/logrus v1.8.1
+)
